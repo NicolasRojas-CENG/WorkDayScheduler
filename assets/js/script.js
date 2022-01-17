f32054ef0dc50ee0d91a283ba0b959c65df0b3e6
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-01-16 22:12:40 -0500

    Completed the edit task without any task value overlaping.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var tasks  = [];
+
 setInterval(function () {
     $("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY, h:mm:ss a"));
 }, 1000);
@@ -6,25 +8,23 @@ var colour = function() {
     var date = moment().hour();
     $(".container").children("article").children("h2").each(function () {
         var value = this.textContent.trim();
-        //value = value.split(" ");
-        //value = value[0];
         var hour = parseInt(value);
         if (hour < 9){
             hour += 12;
         }
         
         if (date > hour){
-            $(this).siblings("input").removeClass('future');
-            $(this).siblings("input").removeClass('present');
-            $(this).siblings("input").addClass('past');
+            $(this).siblings("p").removeClass('future');
+            $(this).siblings("p").removeClass('present');
+            $(this).siblings("p").addClass('past');
         }else if(date < hour) {
-            $(this).siblings("input").removeClass('past');
-            $(this).siblings("input").removeClass('present');
-            $(this).siblings("input").addClass('future');
+            $(this).siblings("p").removeClass('past');
+            $(this).siblings("p").removeClass('present');
+            $(this).siblings("p").addClass('future');
         }else{
-            $(this).siblings("input").removeClass('past');
-            $(this).siblings("input").removeClass('future');
-            $(this).siblings("input").addClass('present');
+            $(this).siblings("p").removeClass('past');
+            $(this).siblings("p").removeClass('future');
+            $(this).siblings("p").addClass('present');
         }
     });
 }
@@ -34,6 +34,49 @@ setInterval(function () {
 }, (1000 * 60) * 5);
 colour();
 
+// $(".description").on("click", function (){
+//     var text = $(this).text().trim();
+//     var textInput = $("<textarea>")
+//      .addClass("col-8 description")
+//      .val(text);
+//      $(this).replaceWith(textInput);
+//      //console.log(textInput);
+// });
 
+// $(".saveBtn").on("click", function() { //ocure cuando apreto el boton que yo le hize click.
+//     var parent = $(this).parent();
+//     var text = parent.find("textarea").val();
+//     var id = parent.attr("id");
+//     //console.log(text);
+//     var textInput = $("<p>")
+//         .addClass("col-8 description")
+//         .text(text);
+//         parent.find("textarea").replaceWith(textInput);
+//     colour();
+//  });
 
-   
+$(".row").on("click", "p", function() {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("col-8 description")
+        .val(text);
+    $(this).replaceWith(textInput);
+});
+
+$(".row").on("click", "button", function() {
+    var parent = $(this).parent();
+    console.log(parent);
+    // var text = parent.find("textarea").val();
+    var id = parent.attr("id");
+    console.log(id);
+    var text = $(this).siblings("textarea")
+       .val()
+       .trim();
+    var taskSpan = $("<p>")
+       .addClass("col-8 description")
+       .text(text);
+    $(this).siblings("textarea").replaceWith(taskSpan);
+    colour();
+});
